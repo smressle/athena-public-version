d73b1bc0d28f2fec49cfba7bb9dd21e37948e2a6
Sean Ressler <smressle@berkeley.edu>
2020-03-31 23:02:01 -0700

    save

--- src/pgen/turb_electrons.cpp
+++ src/pgen/turb_electrons.cpp
@@ -3,53 +3,70 @@
 // Copyright(C) 2014 James M. Stone <jmstone@princeton.edu> and other code contributors
 // Licensed under the 3-clause BSD License, see LICENSE file for details
 //========================================================================================
-//! \file slotted_cylinder.cpp
-//  \brief Slotted cylinder passive scalar advection problem generator for 2D/3D problems.
+//! \file turb.cpp
+//  \brief Problem generator for turbulence generator
 //
-//========================================================================================
+
+// C headers
 
 // C++ headers
-#include <algorithm>  // min, max
-#include <iostream>   // endl
-#include <sstream>    // stringstream
-#include <stdexcept>  // runtime_error
-#include <string>     // c_str()
+#include <cmath>
+#include <ctime>
+#include <sstream>
+#include <stdexcept>
 
 // Athena++ headers
 #include "../athena.hpp"
 #include "../athena_arrays.hpp"
 #include "../coordinates/coordinates.hpp"
 #include "../eos/eos.hpp"
+#include "../fft/athena_fft.hpp"
 #include "../field/field.hpp"
 #include "../globals.hpp"
 #include "../hydro/hydro.hpp"
 #include "../mesh/mesh.hpp"
 #include "../parameter_input.hpp"
-#include "../scalars/scalars.hpp"
-#include "../utils/gl_quadrature.hpp"
+#include "../utils/utils.hpp"
 
-// Parameters which define initial solution -- made global so that they can be shared
-namespace {
-constexpr Real d0 = 1.0;
+#ifdef OPENMP_PARALLEL
+#include <omp.h>
+#endif
+
 Real gm1,gem1,ge,g;
-
-
-} // namespace
 
  void electron_update(MeshBlock *pmb,const Real time, const Real dt,const AthenaArray<Real> &prim, const AthenaArray<Real> &bcc, AthenaArray<Real> &cons);
 
+
+std::int64_t rseed; // seed for turbulence power spectrum
+
 //========================================================================================
 //! \fn void Mesh::InitUserMeshData(ParameterInput *pin)
+//  \brief
 //========================================================================================
-
 void Mesh::InitUserMeshData(ParameterInput *pin) {
+  if (SELF_GRAVITY_ENABLED) {
+    Real four_pi_G = pin->GetReal("problem","four_pi_G");
+    Real eps = pin->GetOrAddReal("problem","grav_eps", 0.0);
+    SetFourPiG(four_pi_G);
+    SetGravityThreshold(eps);
+  }
 
-    EnrollUserExplicitSourceFunction(electron_update);
-
-
+  // turb_flag is initialzed in the Mesh constructor to 0 by default;
+  // turb_flag = 1 for decaying turbulence
+  // turb_flag = 2 for impulsively driven turbulence
+  // turb_flag = 3 for continuously driven turbulence
+  turb_flag = pin->GetInteger("problem","turb_flag");
+  if (turb_flag != 0) {
+#ifndef FFT
+    std::stringstream msg;
+    msg << "### FATAL ERROR in TurbulenceDriver::TurbulenceDriver" << std::endl
+        << "non zero Turbulence flag is set without FFT!" << std::endl;
+    ATHENA_ERROR(msg);
+    return;
+#endif
+  }
   return;
 }
-
 void MeshBlock::InitUserMeshBlockData(ParameterInput *pin) {
 
     gm1 = peos->GetGamma() - 1.0;
@@ -60,48 +77,41 @@ void MeshBlock::InitUserMeshBlockData(ParameterInput *pin) {
 
   return;
 }
-
 //========================================================================================
 //! \fn void MeshBlock::ProblemGenerator(ParameterInput *pin)
-
+//  \brief
 //========================================================================================
 
 void MeshBlock::ProblemGenerator(ParameterInput *pin) {
-  AthenaArray<Real> vol(ncells1);
-
-  // initialize conserved variables
   for (int k=ks; k<=ke; k++) {
     for (int j=js; j<=je; j++) {
-      pcoord->CellVolume(k, j, is, ie, vol);
       for (int i=is; i<=ie; i++) {
-        // background fluid:
+        phydro->u(IDN,k,j,i) = 1.0;
 
-
-        Real x = pcoord->x1v(i);
-        Real L = pcoord->x1f(ie+1) - pcoord->x1f(is);
-        Real xs = pcoord->x1f(is);
-        Real xe = pcoord->x1f(ie+1);
-        Real xmid = xs + L/2.0;
-
-        Real p0 = 1e-4;
-        Real v0 = 1.0;
-
-        if (x > xmid) v0 = -v0;
-        phydro->u(IDN,k,j,i) = d0;
-        phydro->u(IM1,k,j,i) = d0*v0;
+        phydro->u(IM1,k,j,i) = 0.0;
         phydro->u(IM2,k,j,i) = 0.0;
         phydro->u(IM3,k,j,i) = 0.0;
-        // assuming isothermal EOS:
-        phydro->u(IEN,k,j,i) = d0 * SQR(v0)/2.0 + p0/gm1; 
 
-        // set entropy
-        if (NSCALARS > 0) pscalars->s(0,k,j,i) = p0/std::pow(d0,g); //total
-        if (NSCALARS > 1) pscalars->s(1,k,j,i) = p0/std::pow(d0,ge); //electron
-        
+        if (NON_BAROTROPIC_EOS) {
+          phydro->u(IEN,k,j,i) = 1.0;
+        }
+
+
+                // set entropy
+        if (NSCALARS > 0) pscalars->s(0,k,j,i) = 1.0; //total
+        if (NSCALARS > 1) pscalars->s(1,k,j,i) = 0.1; //electron
       }
     }
   }
-  return;
+}
+
+
+//========================================================================================
+//! \fn void Mesh::UserWorkAfterLoop(ParameterInput *pin)
+//  \brief
+//========================================================================================
+
+void Mesh::UserWorkAfterLoop(ParameterInput *pin) {
 }
 
 
@@ -148,5 +158,3 @@ void electron_update(MeshBlock *pmb,const Real time, const Real dt,const AthenaA
   prim_new.DeleteAthenaArray();
   bcc_new.DeleteAthenaArray();
 }
-
-
